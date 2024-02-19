e4efa823d8de2ae61f783f04aad3f103f7ac115d
EdoardoFerrini <edoardo.ferrini@gmail.com>
2024-02-19 19:06:19 +0100

    Aggiunto Register e Login con relative ViewModel e logica Post

---
+++ WhiteLagoon.Web/Controllers/AccountController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+using WhiteLagoon.Web.ViewModel;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WhiteLagoon.Application.Utility;
+namespace WhiteLagoon.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Login(string returnUrl = null)
+        {
+            returnUrl??= Url.Content("~/");
+            LoginVm loginVm = new()
+            {
+                RedirectUrl = returnUrl
+            };
+
+            return View(loginVm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVm loginVm)
+        {
+            if(ModelState.IsValid)
+            {
+                var result = await _signInManager
+                    .PasswordSignInAsync(loginVm.Email, loginVm.Password, loginVm.RememberMe, lockoutOnFailure: false);
+
+                if(result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginVm.RedirectUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        return LocalRedirect(loginVm.RedirectUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login attempt.");
+                }
+
+            }
+            return View(loginVm);
+
+        }
+
+        public IActionResult Register()
+        {
+            if(!_roleManager.RoleExistsAsync(Sd.Role_Admin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(Sd.Role_Admin)).Wait();
+                _roleManager.CreateAsync(new IdentityRole(Sd.Role_Customer)).Wait();
+            }
+
+            RegisterVm registerVm = new()
+            {
+                RoleList = _roleManager.Roles.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Name
+                })
+            };
+
+            return View(registerVm);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVm registerVm)
+        {
+            ApplicationUser user = new()
+            {
+                Name = registerVm.Name,
+                Email = registerVm.Email,
+                PhoneNumber = registerVm.PhoneNumber,
+                NormalizedEmail = registerVm.Email,
+                EmailConfirmed = true,
+                UserName = registerVm.Email,
+                CreatedAt = DateTime.Now,
+            };
+
+            var result = await _userManager.CreateAsync(user, registerVm.Password);
+
+            if (result.Succeeded)
+            {
+                if(!string.IsNullOrEmpty(registerVm.Role)) 
+                {
+                    await _userManager.AddToRoleAsync(user, registerVm.Role);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(user, Sd.Role_Customer);
+                }
+
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                if (string.IsNullOrEmpty(registerVm.RedirectUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    return LocalRedirect(registerVm.RedirectUrl);
+                }
+            }
+
+            foreach(var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            registerVm = new()
+            {
+                RoleList = _roleManager.Roles.Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Name
+                })
+            };
+
+            return View(registerVm);
+        }
+    }
+}
---
+++ WhiteLagoon.Web/ViewModel/RegisterVm.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WhiteLagoon.Web.ViewModel
+{
+    public class RegisterVm
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Phone Number")]
+        public string? PhoneNumber { get; set; }
+        public string? RedirectUrl { get; set; }
+        public string? Role { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem>? RoleList { get; set; }
+    }
+}
